c60cf22e4b0a902c37c4af83bae30271c616c351
Gilberto Freitas <gilberto.ademar@gmail.com>
2019-08-26 03:26:08 -0300

    Adjusting Service Mongo

--- src/service/MongoService.ts
+++ src/service/MongoService.ts
@@ -15,29 +15,29 @@ class MongoService {
         return db.collection(this.nameCollection).find().toArray();
     }
 
-    public async getOneById(value: any) {
+    public async getOneById(value: string) {
         const db: Db = await new MongoWrapper().getConnect();
-        return await db.collection(this.nameCollection).findOne({id: value});
+        return await db.collection(this.nameCollection).findOne({id: value.toString()});
     }
 
     public async getOneByCrm(value: string) {
         const db: Db = await new MongoWrapper().getConnect();
-        return await db.collection(this.nameCollection).findOne({crm: value});
+        return await db.collection(this.nameCollection).findOne({crm: value.toString()});
     }
 
     public async getAllByCrm(value: string) {
         const db: Db = await new MongoWrapper().getConnect();
-        return db.collection(this.nameCollection).find({"medico.crm": value}).toArray();
+        return db.collection(this.nameCollection).find({"medico.crm": value.toString()}).toArray();
     }
 
     public async getAllByCoren(value: string) {
         const db: Db = await new MongoWrapper().getConnect();
-        return db.collection(this.nameCollection).find({"enfermeiros.coren": value}).toArray();
+        return db.collection(this.nameCollection).find({"enfermeiros.coren": value.toString()}).toArray();
     }
 
     public async getOneByCoren(value: string) {
         const db: Db = await new MongoWrapper().getConnect();
-        return await db.collection(this.nameCollection).findOne({coren: value});
+        return await db.collection(this.nameCollection).findOne({coren: value.toString()});
     }
 
     public async insertOne(value: Hospital) {
@@ -49,22 +49,22 @@ class MongoService {
     public async updateOne(valueId: string, value: Hospital) {
         value.id = value.id.toString();
         const db = await new MongoWrapper().getConnect();
-        return await db.collection(this.nameCollection).updateOne({id: valueId}, {$set: value});
+        return await db.collection(this.nameCollection).updateOne({id: valueId.toString()}, {$set: value});
     }
 
     public async deleteOneById(value: string) {
         const db = await new MongoWrapper().getConnect();
-        return await db.collection(this.nameCollection).deleteOne({id: value});
+        return await db.collection(this.nameCollection).deleteOne({id: value.toString()});
     }
 
     public async deleteOneByCrm(value: string) {
         const db = await new MongoWrapper().getConnect();
-        return await db.collection(this.nameCollection).deleteOne({crm: value});
+        return await db.collection(this.nameCollection).deleteOne({crm: value.toString()});
     }
 
     public async deleteOneByCoren(value: string) {
         const db = await new MongoWrapper().getConnect();
-        return await db.collection(this.nameCollection).deleteOne({coren: value});
+        return await db.collection(this.nameCollection).deleteOne({coren: value.toString()});
     }
 }
 
